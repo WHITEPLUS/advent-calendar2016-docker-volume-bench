2d8031287916c9c22c707ebe49043867a60bbf26
kai.zoa <kai.zoa1@gmail.com>
2017-04-07 11:23:19 +0900

    test bind mount consistency

--- bench.sh
+++ bench.sh
@@ -1,6 +1,11 @@
-#! /usr/bin/env bash
+#! /bin/bash -Ceu
 
 [ ! -f ./gophercolor.png ] && \
 curl -O https://golang.org/doc/gopher/gophercolor.png
 
-docker run -v `pwd`:/go/src/bench-fio `docker build --no-cache -q .`
+echo \#consistent
+docker run -v `pwd`:/go/src/bench-fio:consistent `docker build --no-cache -q .`
+echo \#cached
+docker run -v `pwd`:/go/src/bench-fio:cached `docker build --no-cache -q .`
+echo \#delegated
+docker run -v `pwd`:/go/src/bench-fio:delegated `docker build --no-cache -q .`
--- fio_test.go
+++ fio_test.go
@@ -25,23 +25,23 @@ func _read(path string, bufSize int) error {
 	return nil
 }
 
-func BenchmarkReadContainerFile_Buf128(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		err := _read("/tmp/gophercolor.png", 128)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func BenchmarkReadContainerFile_Buf1024(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		err := _read("/tmp/gophercolor.png", 1024)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
+//func BenchmarkReadContainerFile_Buf128(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		err := _read("/tmp/gophercolor.png", 128)
+//		if err != nil {
+//			panic(err)
+//		}
+//	}
+//}
+//
+//func BenchmarkReadContainerFile_Buf1024(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		err := _read("/tmp/gophercolor.png", 1024)
+//		if err != nil {
+//			panic(err)
+//		}
+//	}
+//}
 
 func BenchmarkReadVolumeFile_Buf128(b *testing.B) {
 	for i := 0; i < b.N; i++ {
